8210de338baf6966bdaac54a8b6558aba9eedc64
DavidWei95036593 <dw36593@pausd.us>
2018-02-04 20:27:41 -0800

    finish the thing

--- apppromise.js
+++ apppromise.js
@@ -21,15 +21,38 @@ const argv = yargs
       if(response.data.status ==="ZERO_RESULTS"){
         throw new Error(`Unable to find that address`);
       }else{
+        const lng = response.data.results[0].geometry.location.lng;
         const lag = response.data.results[0].geometry.location.lat;
       console.log(response.data.results[0].formatted_address);
-      console.log(response.data.results[0].geometry.location.lat);
-      console.log(response.data.results[0].geometry.location.lng);
-      // const wethercodeURL = `https://api.darksky.net/forecast/7fa261da4fc6cad0aef6384618ce197e/${response.data.results[0].lagitude},${response.data.results[0].longitude}`
-      // axios.get(wethercodeURL)
-      // .then((response)=>{
-      //     // console.log("hi");
-      //   })
+      // console.log(lng);
+      // console.log(lag);
+      // console.log(response.data.results[0].geometry.location.lat);
+      // console.log(response.data.results[0].geometry.location.lng);
+      const wethercodeURL = `https://api.darksky.net/forecast/7fa261da4fc6cad0aef6384618ce197e/${lag},${lng}`;
+      // console.log(wethercodeURL);
+      axios.get(wethercodeURL)
+      .then((response)=>{
+        if(response.data.status ==="ZERO_RESULTS"){
+          throw new Error(`Unable to find that address`);
+        }else{
+          const timezone = response.data.timezon;
+          const temperature = response.data.currently.temperature;
+          const aptemperature = response.data.currently.apparentTemperature;
+          const visibility = response.data.currently.visibility;
+          const year = new Date().getFullYear();
+          const month = new Date().getMonth()+1;
+          const day = new Date().getDate();
+          const hour = new Date().getHours();
+          const min = new Date().getMinutes();
+          const sec = new Date().getSeconds();
+          console.log(`Your Time zone is ${timezone}`);
+          console.log( `The temperature is ${temperature},and it feels like ${aptemperature}`);
+          console.log(`The visibility is ${visibility}`);
+          console.log(`The current date is ${month}/${day}/${year},the time is ${hour}:${min}:${sec}`);
+          console.log("┻━┻ミ＼(≧ﾛ≦＼)");
+          // console.log(`The weather is ${icon}`);
+        }
+        })
     }
       //sent get request to darksky
       // return axios,get()
