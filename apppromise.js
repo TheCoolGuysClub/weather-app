a323fd6f86dea038b19486c6170da778767218ac
DavidWei95036593 <dw36593@pausd.us>
2018-02-05 10:42:06 -0800

    make it even more readable

--- apppromise.js
+++ apppromise.js
@@ -37,20 +37,20 @@ const argv = yargs
             else{
               const timezone = response.data.timezone;
               const temperature = response.data.currently.temperature;
-            const aptemperature = response.data.currently.apparentTemperature;
-            const visibility = response.data.currently.visibility;
-            const year = new Date().getFullYear();
-            const month = new Date().getMonth()+1;
-            const day = new Date().getDate();
-            const hour = new Date().getHours();
-            const min = new Date().getMinutes();
-            const sec = new Date().getSeconds();
-            console.log(`Your Time zone is ${timezone}`);
-            console.log( `The temperature is ${temperature},and it feels like ${aptemperature}`);
-            console.log(`The visibility is ${visibility}`);
-            console.log(`The current date is ${month}/${day}/${year},the time is ${hour}:${min}:${sec}`);
-            console.log("┻━┻ミ＼(≧ﾛ≦＼)");
-            // console.log(`The weather is ${icon}`);
+              const aptemperature = response.data.currently.apparentTemperature;
+              const visibility = response.data.currently.visibility;
+              const year = new Date().getFullYear();
+              const month = new Date().getMonth()+1;
+              const day = new Date().getDate();
+              const hour = new Date().getHours();
+              const min = new Date().getMinutes();
+              const sec = new Date().getSeconds();
+              console.log(`Your Time zone is ${timezone}`);
+              console.log( `The temperature is ${temperature},and it feels like ${aptemperature}`);
+              console.log(`The visibility is ${visibility}`);
+              console.log(`The current date is ${month}/${day}/${year},the time is ${hour}:${min}:${sec}`);
+              console.log("┻━┻ミ＼(≧ﾛ≦＼)");
+              // console.log(`The weather is ${icon}`);
             }
           })
         }
